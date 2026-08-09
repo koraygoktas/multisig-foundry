893c0a500ad2bdd29ccb480651493f3fabfa0ef0
koraygoktas <krygoktas123@gmail.com>
2026-08-10 01:09:23 +0300

    adding fuzztests

--- test/MultiSigWallet.t.sol
+++ test/MultiSigWallet.t.sol
@@ -58,5 +58,58 @@ function testFuzz_DeploymentSucceedsWithValidConfirmations(uint8 _required) publ
 
     assertEq(newWallet.numConfirmationRequired(), _required);
 }
+function testFuzz_SubmitTransaction(address _to, uint96 _value) public {
+    vm.assume(_to != address(0));
+    vm.assume(_value <= 10 ether);
 
+    vm.prank(owner1);
+    wallet.submitTransaction(_to, _value, "");
+
+    (address to, uint value, , bool executed, uint numConfirmations) = wallet.getTransaction(0);
+
+    assertEq(to, _to);
+    assertEq(value, _value);
+    assertEq(executed, false);
+    assertEq(numConfirmations, 0);
+}
+function testFuzz_ConfirmTransactionIncreasesCount(uint8 _confirmerSeed) public {
+    address[3] memory owners = [owner1, owner2, owner3];
+    address confirmer = owners[_confirmerSeed % 3];
+
+    vm.prank(owner1);
+    wallet.submitTransaction(address(0x99), 1 ether, "");
+
+    vm.prank(confirmer);
+    wallet.confirmTransaction(0);
+
+    (, , , , uint numConfirmations) = wallet.getTransaction(0);
+    assertEq(numConfirmations, 1);
+
+    bool isConfirmed = wallet.isConfirmed(0, confirmer);
+    assertEq(isConfirmed, true);
+}
+function testFuzz_ExecuteTransactionTransfersCorrectAmount(uint96 _value) public {
+    vm.assume(_value > 0 && _value <= 10 ether);
+
+    address recipient = address(0x99);
+    uint recipientBalanceBefore = recipient.balance;
+
+    vm.prank(owner1);
+    wallet.submitTransaction(recipient, _value, "");
+
+    vm.prank(owner1);
+    wallet.confirmTransaction(0);
+
+    vm.prank(owner2);
+    wallet.confirmTransaction(0);
+
+    vm.prank(owner1);
+    wallet.executeTransaction(0);
+
+    uint recipientBalanceAfter = recipient.balance;
+    assertEq(recipientBalanceAfter - recipientBalanceBefore, _value);
+
+    (, , , bool executed, ) = wallet.getTransaction(0);
+    assertEq(executed, true);
+}
 }
